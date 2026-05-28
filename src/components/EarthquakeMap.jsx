5c93140434219b361f09edf18656b19f78b9d7b9
Marc Chretien <marc@marchretien.dev>
2026-05-27 21:02:21 -0400

    style: switch to dark map tiles for consisten theme

--- src/components/EarthquakeMap.jsx
+++ src/components/EarthquakeMap.jsx
@@ -20,8 +20,8 @@ function EarthquakeMap({ quakes }) {
         scrollWheelZoom={true}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="© OpenStreetMap contributors"
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution="© OpenStreetMap contributors © CARTO"
         />
         {quakes.map((quake) => {
           const [lng, lat] = quake.geometry.coordinates;
